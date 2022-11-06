96265394a583266e85318c7bbc7c5c4b195a4e0c
nadira <nadibenham@gmail.com>
2022-11-05 22:44:18 -0400

    worked on the date

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let descriptionElement = document.querySelector("#description");
   let speedElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   speedElement.innerHTML = Math.round(response.data.wind.speed);
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let apikey = "333f5814fa63dat5670a0o088cabaaaf";
 
