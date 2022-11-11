c64366504afa4ed971946f4a5d35e4e6d18a62fe
nadira <nadibenham@gmail.com>
2022-11-10 21:07:10 -0500

    worked on the forecast

--- index.js
+++ index.js
@@ -20,6 +20,81 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp){
+  let date=new Date(timestamp*1000);
+  let day=date.getDay();
+  let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+return days[day];
+}
+function displayForecast(response){
+  let forecast=response.data.daily;
+  let forecastElement=document.querySelector("#forecast");
+  let forecastHTML=`<div class="row">`;
+  forecast.forEach(function(forecastDay,index){
+    if (index<6) {
+      forecastHTML=forecastHTML+`<div class="col-2">
+      <div class="weather-forcast-date">${formatDay(forecastDay.time)}</div>
+      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"
+      alt=""
+      width="42"
+      />
+      <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temperature.maximum)}º</span>
+      <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temperature.minimum)}º</span>
+      </div>
+      </div>`;
+    }
+  });
+ forecastHTML=forecastHTML+`</div>`;
+ forecastElement.innerHTML=forecastHTML;   
+  
+}
+function getForecast(coordinates)
+{
+  let apikey="333f5814fa63dat5670a0o088cabaaaf";
+  let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apikey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let descriptionElement = document.querySelector("#description");
+  let speedElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  fahrenheitTemperature = response.data.temperature.current;
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  speedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.icon);
+  getForecast(response.data.coordinates);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function search(city) {
   let apikey = "333f5814fa63dat5670a0o088cabaaaf";
 
@@ -47,28 +122,6 @@ function displayFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let humidityElement = document.querySelector("#humidity");
-  let descriptionElement = document.querySelector("#description");
-  let speedElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-  fahrenheitTemperature = response.data.temperature.current;
-  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  speedElement.innerHTML = Math.round(response.data.wind.speed);
-
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
-  dateElement.innerHTML = formatDate(response.data.time * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
-  );
-  iconElement.setAttribute("alt", response.data.condition.icon);
-}
 let fahrenheitTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
